8a046b1453334b57e0c8af4afa7984bd158e781d
tigerbeetle <jeremiah.4u@gmail.com>
2019-02-27 11:01:15 +0530

    Feb-27-2019

---
+++ StanLibFunctions/StrlenDemo.c
@@ -0,0 +1,10 @@
+#include <string.h>
+#include <stdio.h>
+
+int main()
+{
+	const char *text = "Hello World, I've been waiting for the chance to see your face.";
+	printf("Text is 		: %s.\n",text);
+	printf("Length of text is 	: %d.\n",strlen(text));
+	return 0;
+}
